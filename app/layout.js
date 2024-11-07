441307f7d263bda377679c058dc2d95d8855d6fa
Syed Md Mihan Chistie <tubbachistie@gmail.com>
2024-11-07 19:39:17 +0530

    init

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,11 @@
-import localFont from "next/font/local";
+import { Poppins } from "next/font/google";
 import "./globals.css";
+import { WagmiProvider } from "@/providers/wagmiProvider";
+import ReduxProvider from "@/providers/reduxProvider";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["200", "300", "400", "500", "600", "700", "800", "900"],
 });
 
 export const metadata = {
@@ -20,10 +16,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={poppins.className}>
+        <ReduxProvider>
+          <WagmiProvider>{children}</WagmiProvider>
+        </ReduxProvider>
       </body>
     </html>
   );
